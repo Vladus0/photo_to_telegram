bd3dc6b2b765bc6daad3df98309849b6d2919465
Vladus0 <118992340+Vladus0@users.noreply.github.com>
2025-01-23 17:43:09 +0400

    publish photo to Telegram channel

--- install_to_telegram.py
+++ install_to_telegram.py
@@ -8,5 +8,7 @@ load_dotenv()
 
 token = os.environ['TOKEN']
 
+chat_id = "@for_test_t"
+
 bot = telegram.Bot(token=token)
-bot.send_message(text='Hi John!', chat_id="@for_test_t")
+bot.send_photo(chat_id=chat_id, photo=open('imgs/nasa_image0.jpg', 'rb'))
